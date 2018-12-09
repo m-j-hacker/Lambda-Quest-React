f7105757bed3b4db8ef3f8344b5fbb980910e175
m-j-hacker <mjhacker@gmail.com>
2018-12-08 23:46:32 -0600

    Refactored code - world now contains player and map components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
-import Map from './features/map';
-import Player from './features/player';
+import World from './features/world';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <Map />
-        <Player />
+        <World />
       </div>
     );
   }
--- src/features/world/index.js
+++ src/features/world/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import Map from '../map';
+import Player from '../player';
 
 function World(props) {
     return (
         <div>
-            
+            <Map />
+            <Player />
         </div>
     )
 }
+
+export default World
