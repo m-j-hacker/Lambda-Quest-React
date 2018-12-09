3844595f8b9dc1971e0088176bc70937186acfe4
m-j-hacker <mjhacker@gmail.com>
2018-12-08 22:52:15 -0600

    Cleaned up App.js error by adding div with dummy data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
-
+import Player from './features/player';
 
 class App extends Component {
   render() {
     return (
-      
+      <div>Awesome game</div>
     );
   }
 }
