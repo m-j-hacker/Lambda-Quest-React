914e4c1dcd51c0b54c451dccf0fc35c693f6450b
m-j-hacker <mjhacker@gmail.com>
2018-12-09 01:55:49 -0600

    Added tiles for map, updated constants, began to fill out tile code

--- src/features/world/index.js
+++ src/features/world/index.js
@@ -2,10 +2,20 @@ import React from 'react'
 import Map from '../map';
 import Player from '../player';
 
+import { tiles } from '../../data/map/1'
+
 function World(props) {
     return (
-        <div>
-            <Map />
+        <div
+            style={{
+                position: 'relative',
+                width: '800px',
+                height: '400px',
+                margin: '20px auto',
+            }}
+        >
+
+            <Map tiles={tiles} />
             <Player />
         </div>
     )
